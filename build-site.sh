270151317c30078ec83a07dd464703ec8d5d3b52
Crementif <26669564+Crementif@users.noreply.github.com>
2022-02-06 18:25:02 +0100

    Change to Cloudflare CDN for payload.elf

--- build-site.sh
+++ build-site.sh
@@ -12,5 +12,6 @@ php index.php > ./../docs/index.html
 php exploit.php > ./../docs/exploit.html
 php 404.php > ./../docs/404.html
 cp index.js ./../docs/index.js
+cp dumpling-logo.png ./../docs/dumpling-logo.png
 cp payload.elf ./../docs/payload.elf
 cd ..
