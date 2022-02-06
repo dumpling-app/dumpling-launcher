d50ddf93136138f14f19459b253346a1813829a0
Crementif <26669564+Crementif@users.noreply.github.com>
2022-02-06 18:37:06 +0100

    Fix for TLS version

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define CONFIG_PAYLOAD_URL "https://dumplingapp.com/payload.elf"
+#define CONFIG_PAYLOAD_URL "https://payload.dumpling-wiiu.workers.dev"
 #define CONFIG_RPX_URL "https://dumpling.dumpling-wiiu.workers.dev"
